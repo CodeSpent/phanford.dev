03b6db315c0d671b01185be52f2dd8eb80f2742f
CodeSpent <pat@codespent.dev>
2022-09-13 21:11:54 -0400

    refactor: start on new hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,6 +22,10 @@ const taglines = [
   "Solutions",
 ];
 
+const getRandomLineWidth = (min, max) => {
+  return `w-${Math.floor(Math.random() * (max - min + 1) + min)}`;
+};
+
 const HeroComponent = () => {
   const [tagline, setTagline] = useState("Solutions");
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -37,47 +41,106 @@ const HeroComponent = () => {
   }, [currentIndex]);
 
   return (
-    <div className="h-screen--nav-aware flex flex-col justify-center p-4 leading-6 tracking-widest">
+    <div className="flex h-screen flex-col justify-center p-4 leading-6 tracking-widest">
       <div className="min-h-max" style={{ minHeight: "200px" }}>
-        <span className="border-b border-orange-500 text-xl font-medium italic text-gray-300">
-          Hi, my name is
-        </span>
-        <h1 className="text-6xl text-white lg:text-9xl">
-          Patrick
-          <span className="bg-gradient-to-r from-orange-500 to-yellow-500  bg-clip-text text-transparent">
-            .
-          </span>
-        </h1>
+        <div className="my-10 flex flex-col gap-3">
+          <div className="flex h-4 gap-3">
+            <div
+              className={`w-48 rounded bg-gradient-to-l from-blue-500`}
+            ></div>
 
-        <Transition
-          enter-active-class="transition-opacity duration-75"
-          enter-from-class="opacity-0"
-          enter-to-class="opacity-100"
-          leave-active-class="transition-opacity duration-150"
-          leave-from-class="opacity-100"
-          leave-to-class="opacity-0"
-          show={true}
-        >
-          <h2 className="text-4xl font-thin text-gray-300 lg:text-6xl">
-            I build{" "}
-            <span className="bg-gradient-to-r from-yellow-500 to-orange-500 bg-clip-text px-2 font-bold italic text-transparent">
-              {tagline}
+            <div
+              className={
+                "w-48 rounded bg-gradient-to-r from-orange-500 to-orange-500"
+              }
+            ></div>
+
+            <div className="w-4 rounded bg-gradient-to-r from-pink-500 to-red-500 "></div>
+          </div>
+
+          <div className="flex h-4 gap-3">
+            <div className="transparent w-14"></div>
+            <div className="w-12 rounded bg-gradient-to-r from-blue-400 to-blue-500"></div>
+            <div className="w-36 rounded bg-gradient-to-r from-green-400 to-green-500"></div>
+          </div>
+
+          <div className="flex h-4 gap-3">
+            <div className="transparent w-24"></div>
+            <div className="w-12 rounded bg-gradient-to-r from-green-400 to-green-500"></div>
+            <div className="transparent w-4"></div>
+
+            <div className="w-12 rounded bg-gradient-to-r from-pink-500 to-red-500"></div>
+            <div className="w-72 rounded bg-gradient-to-r from-yellow-500 to-orange-500"></div>
+            <div className="w-36 rounded bg-gradient-to-r from-green-400 to-green-500"></div>
+          </div>
+        </div>
+
+        <div className="ml-16">
+          <div className="flex h-4 gap-3">
+            <span className="-mt-1.5 text-lg font-semibold italic text-gray-300">
+              Hi, my name is
             </span>
-          </h2>
-        </Transition>
-      </div>
+            <div className="transparent w-36"></div>
+            <div className="w-24 rounded bg-gradient-to-r from-green-500 to-green-500"></div>
+            <div className="w-4 rounded bg-gradient-to-r from-red-500 to-red-500"></div>
+          </div>
 
-      <p className="mt-4 max-w-lg rounded border border-gray-900 p-6 font-thin backdrop-blur-sm lg:max-w-4xl">
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using 'Content here, content here', making it
-        look like readable English. Many desktop publishing packages and web
-        page editors now use Lorem Ipsum as their default model text, and a
-        search for 'lorem ipsum' will uncover many web sites still in their
-        infancy. Various versions have evolved over the years, sometimes by
-        accident, sometimes on purpose (injected humour and the like).
-      </p>
+          <h1 className="text-6xl text-white lg:text-8xl">Patrick Hanford</h1>
+          <Transition
+            enter-active-class="transition-opacity duration-75"
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+            leave-active-class="transition-opacity duration-150"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+            show={true}
+          >
+            <h2 className="text-4xl font-thin italic text-gray-300 lg:text-6xl">
+              I build <span className="font-semibold">{tagline}</span>
+              <span className="font-semibold italic text-white">.</span>
+            </h2>
+          </Transition>
+        </div>
+
+        <div className="my-10 flex flex-col gap-3">
+          <div className="flex h-4 gap-3">
+            <div className="w-24 rounded bg-gradient-to-l from-blue-500"></div>
+            <div className="w-12 rounded bg-gradient-to-r from-yellow-500 to-orange-500"></div>
+            <div className="transparent w-36"></div>
+            <div className="w-36 rounded bg-gradient-to-r from-green-400 to-green-500"></div>
+          </div>
+
+          <div className="flex h-4 gap-3">
+            <div className="transparent w-14"></div>
+            <div className="w-36 rounded bg-gradient-to-r from-blue-400 to-blue-600"></div>
+            <div className="w-12 rounded bg-gradient-to-r from-pink-500 to-red-500"></div>
+            <div className="w-48 rounded bg-gradient-to-r from-yellow-500 to-orange-600"></div>
+          </div>
+          <div className="flex h-4 gap-3">
+            <div className="transparent w-14"></div>
+            <div className="w-12 rounded bg-gradient-to-r from-pink-500 to-red-500"></div>
+            <div className="w-24 rounded bg-gradient-to-r from-blue-400 to-blue-600"></div>
+            <div className="w-48 rounded bg-gradient-to-r from-yellow-500 to-orange-600"></div>
+          </div>
+
+          <div className="flex h-4 gap-3"></div>
+
+          <div className="flex h-4 gap-3">
+            <div className="w-12 rounded bg-gradient-to-l from-orange-600"></div>
+            <div className="w-36 rounded bg-gradient-to-r from-green-400 to-green-600"></div>
+            <div className="w-4 rounded bg-gradient-to-r from-blue-400 to-blue-500"></div>
+            <div className="w-24 rounded bg-gradient-to-r from-purple-500 to-red-500"></div>
+          </div>
+
+          <div className="flex h-4 gap-3">
+            <div className="transparent w-14"></div>
+            <div className="w-12 rounded bg-gradient-to-r from-yellow-400 to-yellow-500"></div>
+            <div className="w-4 rounded bg-gradient-to-r from-green-500 to-green-500"></div>
+            <div className="w-72 rounded bg-gradient-to-r from-red-500 to-purple-500"></div>
+            <div className="w-24 rounded bg-gradient-to-r from-blue-500 to-blue-600"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
