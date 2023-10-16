9e368f7a0c9d2a21070f5a089aaa34303d3c4c69
CodeSpent <pat@codespent.dev>
2023-10-16 17:05:28 -0400

    hotfix: fix footer social links

--- components/common/navbars/Footer.tsx
+++ components/common/navbars/Footer.tsx
@@ -4,14 +4,13 @@ import SocialLinks from '../../SocialLinks'
 export default function Footer() {
   return (
     <footer className="">
-      <div className="mx-auto max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl overflow-hidden px-4 py-12 sm:px-6 lg:px-8">
         <div className="mt-8 flex justify-center space-x-6">
-          <SocialLinks />
+          <SocialLinks className="flex gap-3" />
         </div>
 
         <p className="mt-8 text-center text-base text-gray-400">
-          &copy; {new Date().getFullYear()} Patrick Hanford | CodeSpent, LLC.
-          All rights reserved.
+          &copy; {new Date().getFullYear()} Patrick Hanford | CodeSpent, LLC. All rights reserved.
         </p>
       </div>
     </footer>
