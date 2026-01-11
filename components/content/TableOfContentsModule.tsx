572d00aaf4aefb76f91c2c8017bdd5cb50355709
CodeSpent <hanfordpatrick@gmail.com>
2026-01-10 21:36:01 -0500

    fix: fix toc sizing

--- components/content/TableOfContentsModule.tsx
+++ components/content/TableOfContentsModule.tsx
@@ -11,14 +11,12 @@ interface TableOfContentsModuleProps {
 /**
  * Table of Contents wrapped in a sidebar module
  * Reuses existing TableOfContents component for consistency
- *
- * Note: Combines top-40 with -mt-2 for fine-tuned positioning below navbar
  */
 export const TableOfContentsModule: React.FC<TableOfContentsModuleProps> = ({
   className = '',
 }) => {
   return (
-    <div className={`sticky top-40 -mt-2 ${className}`}>
+    <div className={`sticky top-24 ${className}`}>
       <TableOfContents />
     </div>
   )
